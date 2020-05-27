afaf7ac3a6b370c115ef43a9972ac8fb770bb02d
Ari <ariannasg.ldn@gmail.com>
2020-05-27 16:33:08 +0100

    Added classes with dummy tests to understand test discovery

---
+++ examples/test_my_class.py
@@ -0,0 +1,31 @@
+#!usr/bin/env python3
+
+
+class TestMyClass:
+    def test_my_class_1(self):
+        assert True
+
+    def test_my_class_2(self):
+        assert True
+
+    def my_class_3_test(self):
+        assert True
+
+    def my_class_test_4(self):
+        assert True
+
+
+class MyTestClass2:
+    def test_my_class_1(self):
+        assert True
+
+    def test_my_class_2(self):
+        assert True
+
+
+class MyClass2Test:
+    def test_my_class_1(self):
+        assert True
+
+    def test_my_class_2(self):
+        assert True
