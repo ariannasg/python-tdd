d8cabc4be03726c92e4903e2eb11985e1e6d8c22
Ari <ariannasg.ldn@gmail.com>
2020-06-03 08:02:45 +0100

    Refactored class

--- fizz_buzz_kata/test_fizz_buzz.py
+++ fizz_buzz_kata/test_fizz_buzz.py
@@ -5,7 +5,7 @@ import pytest
 def fizz_buzz(value):
     if is_multiple(value, 3):
         if is_multiple(value, 5):
-            return "fizz_buzz"
+            return "Fizz_buzz"
         return "Fizz"
     if is_multiple(value, 5):
         return "Buzz"
@@ -16,38 +16,6 @@ def is_multiple(value, mod):
     return (value % mod) == 0
 
 
-def check_fizz_buzz(value, expected_value):
-    assert fizz_buzz(value) == expected_value
-
-
-def test_returns_1_with_1_passed_in():
-    check_fizz_buzz(1, "1")
-
-
-def test_returns_2_with_2_passed_in():
-    check_fizz_buzz(2, "2")
-
-
-def test_returns_fizz_with_3_passed_in():
-    check_fizz_buzz(3, "Fizz")
-
-
-def test_returns_buzz_with_5_passed_in():
-    check_fizz_buzz(5, "Buzz")
-
-
-def test_returns_fizz_with_6_passed_in():
-    check_fizz_buzz(6, "Fizz")
-
-
-def test_returns_buzz_with_10_passed_in():
-    check_fizz_buzz(10, "Buzz")
-
-
-def test_returns_fizz_buzz_with_15_passed_in():
-    check_fizz_buzz(15, "fizz_buzz")
-
-
 fizz_buzz_test_data = [
     (1, "1"),
     (2, "2"),
@@ -55,7 +23,7 @@ fizz_buzz_test_data = [
     (5, "Buzz"),
     (6, "Fizz"),
     (10, "Buzz"),
-    (15, "fizz_buzz"),
+    (15, "Fizz_buzz"),
 ]
 
 
